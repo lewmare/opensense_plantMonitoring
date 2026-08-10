a396123b7ded6157c85ddbc5f941748ef38d0797
lewmare <lewmare0@gmail.com>
2026-08-10 19:45:15 +0000

    new .gitignore

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-// secrets.h
-// Simpan file ini terpisah dari source code utama dan JANGAN di-commit ke Git
-// (tambahkan "secrets.h" ke .gitignore).
-#pragma once
-
-#define WIFI_SSID     "RichMeal"
-#define WIFI_PASSWORD "AkhdanRafif0621"
-#define SERVER_URL    "http://192.168.1.10:3001/api/sensor"
